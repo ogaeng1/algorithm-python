80d6888093aaee5cd13f8660104866b4422bbaa8
ogaeng1 <59693688+ogaeng1@users.noreply.github.com>
2023-11-06 02:15:19 +0900

    [Silver V] Title: 너의 평점은, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.py
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -1,36 +1,13 @@
 score = 0
 sum = 0
 
-for _  in range(20):
-  info = input().split()
-  if info[2] == "A+":
-    score += float(info[1]) * 4.5
-    sum += float(info[1])
-  elif info[2] == "A0":
-    score += float(info[1]) * 4
-    sum += float(info[1])
-  elif info[2] == "B+":
-    score += float(info[1]) * 3.5
-    sum += float(info[1])
-  elif info[2] == "B0":
-    score += float(info[1]) * 3
-    sum += float(info[1])
-  elif info[2] == "C+":
-    score += float(info[1]) * 2.5
-    sum += float(info[1])
-  elif info[2] == "C0":
-    score += float(info[1]) * 2
-    sum += float(info[1])
-  elif info[2] == "D+":
-    score += float(info[1]) * 1.5
-    sum += float(info[1])
-  elif info[2] == "D0":
-    score += float(info[1]) * 1.0
-    sum += float(info[1])
-  elif info[2] == "F":
-    score += 0
-    sum += float(info[1])
-  elif info[2] == "P":
-    continue
+info = {"A+": 4.5, "A0": 4.0, "B+": 3.5, "B0": 3.0, "C+": 2.5, "C0": 2.0, "D+": 1.5, "D0": 1.0, "F": 0.0}
 
-print(score / sum)
+for _ in range(20):
+    subject, credit, grade = input().split()
+    if grade == "P":
+        continue
+    score += float(credit) * info[grade]
+    sum += float(credit)
+    
+print(score/sum)
