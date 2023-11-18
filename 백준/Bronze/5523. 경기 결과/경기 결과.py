62c24f2f96857a017fa81d695fa5335d4a5fe699
ogaeng1 <59693688+ogaeng1@users.noreply.github.com>
2023-11-18 15:36:03 +0900

    [Bronze III] Title: 경기 결과, Time: 876 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/5523. 경기 결과/경기 결과.py
+++ 백준/Bronze/5523. 경기 결과/경기 결과.py
@@ -1,13 +1,12 @@
-N = int(input())
+import sys
+N = int(sys.stdin.readline())
 A, B = 0, 0
 
 for _ in range(N):
-  score = list(map(int, input().split()))
-  if score[0] > score[1]:
+  a, b = map(int, sys.stdin.readline().split())
+  if a > b:
     A += 1
-  elif score[0] < score[1]:
+  elif a < b:
     B += 1
-  else:
-    pass
 
 print(A, B)
