65181adc7f8f176cb5132500d6ba80f9263d31aa
ogaeng1 <59693688+ogaeng1@users.noreply.github.com>
2023-11-22 04:10:42 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.py
@@ -0,0 +1,10 @@
+guho = input()
+
+if guho == "SONGDO":
+  print("HIGHSCHOOL")
+elif guho == "CODE":
+  print("MASTER")
+elif guho == "2023":
+  print("0611")
+elif guho == "ALGORITHM":
+  print("CONTEST")
