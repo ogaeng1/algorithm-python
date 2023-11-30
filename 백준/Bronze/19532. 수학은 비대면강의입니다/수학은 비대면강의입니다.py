c240783692169c11c617b33b074987e0c95b1924
ogaeng1 <59693688+ogaeng1@users.noreply.github.com>
2023-11-30 22:11:27 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 696 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
@@ -1,13 +1,6 @@
 a, b, c, d, e, f = map(int, input().split())
-x, y = 0, 0
 
-for i in range(-999, 1000, 1):
-  for j in range(-999, 1000, 1):
+for i in range(-999, 1000):
+  for j in range(-999, 1000):
     if (a * i) + (b * j) == c and (d * i) + (e * j) == f:
-      x = i
-      y = j
-      break
-    else:
-      continue
-
-print(x, y)
+      print(i, j)
