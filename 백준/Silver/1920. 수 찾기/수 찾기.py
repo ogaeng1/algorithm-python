d6943f3837f0b747e7e6a42c5800255e9ca2bb8f
ogaeng1 <59693688+ogaeng1@users.noreply.github.com>
2023-12-01 17:05:28 +0900

    [Silver IV] Title: 수 찾기, Time: 456 ms, Memory: 46720 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,24 @@
+def bi_search(n_list, target):
+  start, end = 0, len(n_list)-1
+
+  while start <= end:
+    mid = (start + end) // 2
+    if n_list[mid] == target:
+      return 1
+    elif n_list[mid] > target:
+      end = mid - 1
+    else:
+      start = mid + 1
+
+  return 0
+
+n =int(input())
+n_list = list(map(int, input().split()))
+m = int(input())
+m_list = list(map(int, input().split()))
+
+n_list.sort()
+
+for i in m_list:
+  res = bi_search(n_list, i)
+  print(res)
