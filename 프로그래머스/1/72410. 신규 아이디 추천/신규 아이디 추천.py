22bb392ab9d884082aedc089c9a63d596f5ac160
ogaeng1 <59693688+ogaeng1@users.noreply.github.com>
2023-12-04 03:12:11 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.20 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -0,0 +1,27 @@
+def solution(new_id):
+    answer = ''
+    new_id = new_id.lower()
+    
+    for i in new_id:
+        if i.isalpha() or i.isdigit() or i in '-_.':
+            answer += i
+            
+    while '..' in answer:
+        answer = answer.replace('..', '.')
+    
+    if answer[0:1] == '.':
+        answer = answer[1:]
+    if answer[-1:0] == '.':
+        answer = answer[:-1]
+        
+    if answer == '':
+        answer = 'a'
+    
+    answer = answer[:15]
+    if answer[-1] == '.':
+        answer = answer[:-1]
+        
+    while len(answer) < 3:
+        answer += answer[-1]
+        
+    return answer
