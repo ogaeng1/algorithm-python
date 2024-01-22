3cfbbfdb4b91cc2408e621fa2eed618a1895eab2
ogaeng1 <59693688+ogaeng1@users.noreply.github.com>
2024-01-22 15:28:37 +0900

    [Silver III] Title: 퇴사, Time: 60 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -0,0 +1,18 @@
+n = int(input())
+day, money = [], []
+dp = [0] * (n+1)
+result = 0
+
+for _ in range(n):
+  t, p = map(int, input().split())
+  day.append(t)
+  money.append(p)
+
+for i in range(n-1, -1, -1):
+  if day[i] + i <= n:
+    dp[i] = max(money[i] + dp[day[i]+i], result)
+    result = dp[i]
+  else:
+    dp[i] = result
+
+print(result)
