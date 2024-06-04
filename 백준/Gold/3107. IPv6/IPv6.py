5f61464b078c2b29b2e88023ff5c388b9542c9a3
ogaeng1 <01092428351a@gmail.com>
2024-06-04 11:57:30 +0900

    [Gold V] Title: IPv6, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/3107. IPv6/IPv6.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+ipv6 = input().strip()
+
+if ipv6.count(":") == 8:
+  ipv6 = ipv6.replace("::", ":")
+
+ipv6 = ipv6.replace("::", (7-ipv6.count(":"))*":" + "::")
+ipv6 = ipv6.split(":")
+
+for i in range(len(ipv6)):
+  if len(ipv6[i]) < 4:
+    ipv6[i] = (4-len(ipv6[i]))*'0' + ipv6[i]
+    
+print(":".join(ipv6))
