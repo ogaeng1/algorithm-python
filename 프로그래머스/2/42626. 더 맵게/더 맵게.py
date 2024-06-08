5a2c7ba97ac59f7ce162adf7694da94a4aa6c808
ogaeng1 <01092428351a@gmail.com>
2024-06-09 00:09:18 +0900

    [level 2] Title: 더 맵게, Time: 1655.36 ms, Memory: 51.9 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -4,7 +4,7 @@ def solution(scoville, K):
     heapq.heapify(scoville)
     res = 0
     
-    while scoville and scoville[0] < K and len(scoville) > 1:
+    while scoville[0] < K and len(scoville) > 1:
         min_scov1 = heapq.heappop(scoville)
         min_scov2 = heapq.heappop(scoville)
         mix_scov = min_scov1 + (min_scov2 * 2)
