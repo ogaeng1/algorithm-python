1da9327d2e2c7bdb8f59aae3a7899f21f4d504c2
Kyung Min OH <01092428351a@gmail.com>
2025-01-03 23:36:42 +0900

    [Silver IV] Title: 좋은 단어, Time: 112 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.py
@@ -0,0 +1,17 @@
+n = int(input())
+cnt = 0
+
+for _ in range(n):
+  stack = []
+  word = input().strip()
+
+  for i in word:
+    if stack and stack[-1] == i:
+        stack.pop()
+    else:
+      stack.append(i)
+
+  if not stack:
+    cnt += 1
+
+print(cnt)
